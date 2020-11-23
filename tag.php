a47ad3a32598002130c9d2e22702134d89914d59
Baux Aurélien <baux.aurelien62@gmail.com>
2020-11-23 17:30:16 +0100

    Ajout d'une page qui montre les balises php
    Cette page permet de montrer quand l'on doit utiliser la balise php fermante.

---
+++ tag.php
@@ -0,0 +1,20 @@
+<?php
+
+// code php
+
+?><!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <p><?php echo 'lorem ipsum' ?></p>
+    <p><?= 'lorem ipsum' ?></p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos officia qui magni quidem eligendi doloribus, sed dolorum quaerat provident temporibus fugiat dicta? Commodi molestias suscipit harum illo ratione excepturi? Laborum.</p>
+</body>
+</html>
+<?php
+
+// pas de balise fermante si votre code php termine le fichier
